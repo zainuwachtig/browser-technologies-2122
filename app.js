0e2d6777a5186acf4a529ea5c9d29c081d2acfea
zainuwachtig <zain.butt@hva.nl>
2022-03-31 19:15:13 +0200

    serverside en html opgezet

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const fs = require('fs')
+
+const app = express();
+
+const hostname = '127.0.0.1';
+const port = 5505;
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
+app.set('view engine', 'ejs');
+const ejs = require('ejs');
+app.use(express.static('static'))
+
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+app.post('/winkelmandje', (req, res) => {
+    res.render('winkelmandje');
+    console.log(req['body']['kleur-options'], req['body']['maat-options'], req['body']['text-options'])
+});
+
+app.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+});
