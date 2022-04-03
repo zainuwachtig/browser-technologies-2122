b64ebbfbe0d6e155e15f36f195ade0eff8937f11
zainuwachtig <zain.butt@hva.nl>
2022-04-03 18:36:42 +0200

    kleur op tee gefixt

--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,26 @@
+// Verandert de kleur van de tee
+function changeColorOnTee(color) {
+    // Selecteert alle stukjes uit de tee
+    const tee = document.querySelectorAll('.st1');
+    
+    tee.forEach((el) => {
+        el.style.fill = color
+    })
+}
+
+// Gekozen kleur op de tee zien
+function getColorOnTee() {
+    const colorPicker  = document.querySelectorAll('fieldset:nth-of-type(2) input')
+    
+    colorPicker.forEach((color) => {
+        color.addEventListener('change', (e) => {
+            // Value tekst is in engels zodat css die herkent
+            changeColorOnTee(e.target.value)
+        })
+    })
+    
+}
+
 // Ingevoerde tekst op de tee zien
 function getTextOnTee() {
     const textInput = document.querySelector('.text-wrapper input');
@@ -8,4 +31,5 @@ function getTextOnTee() {
     })
 }
 
+getColorOnTee()
 getTextOnTee()
